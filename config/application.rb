a427b8fdec769f0a0f5e7fe49371e5a0fc5fefa4
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-07-01 12:17:35 +0200

    Remove action mailer, action cable and unused files

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+# require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
@@ -6,9 +6,9 @@ require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
