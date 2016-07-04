f63d62568efef62b0ca2839dd73503048a2b07e4
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-07-04 16:27:08 +0200

    Enable error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-  # rescue_from Exception do |e|
-  #   error(e)
-  # end
+  rescue_from Exception do |e|
+    error(e)
+  end
 
-  # def routing_error
-  #   raise ActionController::RoutingError.new(params[:path])
-  # end
+  def routing_error
+    raise ActionController::RoutingError.new(params[:path])
+  end
 
   protected
 
-    # def error(e)
-    #   if request.env["ORIGINAL_FULLPATH"] =~ /^\//
-    #     error_info = { error: "internal-server-error", exception: "#{e.class.name} : #{e.message}" }
-    #     error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
-    #     render json: error_info.to_json, status: 500
-    #   else
-    #     raise e
-    #   end
-    # end
+    def error(e)
+      if request.env["ORIGINAL_FULLPATH"] =~ /^\//
+        error_info = { error: "internal-server-error", exception: "#{e.class.name} : #{e.message}" }
+        error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
+        render json: error_info.to_json, status: 500
+      else
+        raise e
+      end
+    end
 
     def basic_auth
       authenticate_or_request_with_http_basic('Administration') do |user, password|
